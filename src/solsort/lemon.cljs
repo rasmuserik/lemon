cfaa12ecfaadc894e4f521a0882851932c777036
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-09-08 19:14:12 +0200

    documentation

--- src/solsort/lemon.cljs
+++ src/solsort/lemon.cljs
@@ -2,20 +2,53 @@
 ;;
 ;; This repository will contain widgets and apps for Tinkuy/NewCircleMovement/...
 ;;
+;; It is currently in very initial development, and not really usable for anything yet.
+;; More info will follow later.
+;;
+;; ## Development environment / Getting started
+;;
+;; Install leiningen, and:
+;;
+;;     lein figwheel
+;; 
+;; Then a local development-version of the NewCircleMovement/tinkuy ruby app, 
+;; running on port 3000, will connect directly to the clojurescript environment,
+;; with repl-support with the tinkuy-site.
+;;
 ;; # Literate source code
-
+;;
+;; I like the concept of 
+;; [literate programming](https://en.wikipedia.org/wiki/Literate_programming),
+;; where the code is written as a document to be read by humans too.
+;; In the following there will be the actual code, intermixed with a description
+;; of the ideas behind it.
+;;
+;; ## Namespace definition
+;;
+;; Define the module, and declare the dependencies. Use the standard ClojureScript modules
 (ns solsort.lemon
   (:require-macros
     [reagent.ratom :as ratom :refer [reaction]]
     [cljs.core.async.macros :refer  [go alt!]])
-
   (:require
     [cljs.test :refer-macros  [deftest testing is run-tests]]
-    [solsort.util :refer [route log unique-id]]
-    [re-frame.core :as re-frame :refer [subscribe]]
-    [cljs.core.async :refer [>! <! chan put! take! timeout close!]]))
+    [cljs.core.async :refer [>! <! chan put! take! timeout close!]]
 
-;; ## example/test-app
+;; It uses the re-frame framework.
+;;
+;; If you are interested in client-side development in general, 
+;; read the [re-frame](https://github.com/Day8/re-frame) readme.
+;; As that is a very good document about how to structure application.
+    [re-frame.core :as re-frame :refer [subscribe]]
+
+;; And some of my own utility functions, that I share among projects.
+;; Routing, platform-abstraction, utilities, etc.
+    [solsort.util :refer [route log unique-id]]
+    ))
+
+;; ## Sample/getting started code
+;;
+;; This is just a small hello-world app, will be replaced by the actual code soon.
 (route
   "lemon" :app
   (fn []
